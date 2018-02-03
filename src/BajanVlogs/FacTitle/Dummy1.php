b95ae3f63624b84b6083db0747b593bac27d063d
iZeaoGamer <jameshayward14@icloud.com>
2018-02-03 17:29:41 +0000

    Fixed Syntax error.

--- src/BajanVlogs/FacTitle/Dummy1.php
+++ src/BajanVlogs/FacTitle/Dummy1.php
@@ -1,42 +1,17 @@
 <?php
 
-
 namespace BajanVlogs\FacTitle;
 
-
-
 use pocketmine\Server;
-
-
 use pocketmine\Player;
-
-
-
 use BajanVlogs\FacTitle\Main;
 
-
-
-
-
-
-
 class Dummy {
-
-
 
 
    public function __construct(Main $main) {
 
 
         $this->main = $main;
-
-
-        $this->server = $main->getServer()
-
-
-    }
-
-
-
-
+        $this->server = $main->getServer();
 }
